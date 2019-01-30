6108138435b68bac473cef74db382cb9c67c524e
tlehtikallio <tlehtikallio@gmail.com>
2019-01-30 14:42:37 +0200

    movement improved, cannot go over top, left and bottom border. Can still go over right border

--- RogueLikeConsoleGame/Movement.cs
+++ RogueLikeConsoleGame/Movement.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
-
 namespace RogueLikeConsoleGame
 {
     public class Movement // Timo Lehtikallio 29/1/2019
@@ -13,98 +12,63 @@ namespace RogueLikeConsoleGame
         public static int NewPlayerPositionLeft;
         public static int OldPlayerPositionTop;
         public static int OldPlayerPositionLeft;
-
         public static void StartPosition()
         {
             CurrentPlayerPositionTop = Console.CursorTop = 10;
             CurrentPlayerPositionLeft = Console.CursorLeft = 20;
             Console.Write("@");
         }
-
-        /*public static void TestArena()
-        {
-            char[,] arena2d = new char[2, 2] { { '.', '.' }, { '.', '.' } };
-            Console.Write(arena2d[0, 0]);
-            Console.WriteLine(arena2d[0, 1]);
-            Console.Write(arena2d[1, 0]);
-            Console.Write(arena2d[1, 1]);
-        }*/
-
+        
         public static void Move()
         {
+            Console.CursorVisible = false;
             StartPosition();
             Arena.CreateArena();
-            //TestArena();
-
             var input = Console.ReadKey();
-
             do
             {
                 input = Console.ReadKey();
                 OldPlayerPositionTop = CurrentPlayerPositionTop;
                 OldPlayerPositionLeft = CurrentPlayerPositionLeft;
+
                 switch (input.Key)
                 {
                     case ConsoleKey.UpArrow: // move.Y -= 1;
                         NewPlayerPositionTop = CurrentPlayerPositionTop - 1;
                         NewPlayerPositionLeft = CurrentPlayerPositionLeft;
                         break;
-
                     case ConsoleKey.LeftArrow: // move.X -= 1;
                         NewPlayerPositionTop = CurrentPlayerPositionTop;
                         NewPlayerPositionLeft = CurrentPlayerPositionLeft - 1;
                         break;
-
                     case ConsoleKey.DownArrow: // move.Y += 1;
                         NewPlayerPositionTop = CurrentPlayerPositionTop + 1;
                         NewPlayerPositionLeft = CurrentPlayerPositionLeft;
                         break;
-
                     case ConsoleKey.RightArrow: // move.X += 1;
                         NewPlayerPositionTop = CurrentPlayerPositionTop;
                         NewPlayerPositionLeft = CurrentPlayerPositionLeft + 1;
                         break;
                 }
 
-                CurrentPlayerPositionTop = NewPlayerPositionTop;
-                CurrentPlayerPositionLeft = NewPlayerPositionLeft;
+                bool wall = Arena.IsWall(NewPlayerPositionLeft, NewPlayerPositionTop);
 
-                Console.CursorTop = OldPlayerPositionTop;
-                Console.CursorLeft = OldPlayerPositionLeft;
-                Console.Write(" ");
-
-                if (CurrentPlayerPositionLeft == Arena.MonsterPositionLeft && CurrentPlayerPositionTop == Arena.MonsterPositionTop)
+                if (wall)
                 {
-                    Console.WriteLine("Wololoo!");
-                    //Player.Attack();
+                    continue;
                 }
-
-                if (CurrentPlayerPositionLeft == Arena.ChestPositionLeft && CurrentPlayerPositionTop == Arena.ChestPositionTop)
+                if (!wall)
                 {
-                    Console.WriteLine("HeiHei");
-                    //Player.Loot();
+                    CurrentPlayerPositionTop = NewPlayerPositionTop;
+                    CurrentPlayerPositionLeft = NewPlayerPositionLeft;
+                    Console.CursorTop = OldPlayerPositionTop;
+                    Console.CursorLeft = OldPlayerPositionLeft;
+                    Console.Write(" ");
+                    Console.CursorTop = CurrentPlayerPositionTop;
+                    Console.CursorLeft = CurrentPlayerPositionLeft;
+                    Console.Write("@");
                 }
-
-                Console.CursorTop = CurrentPlayerPositionTop;
-                Console.CursorLeft = CurrentPlayerPositionLeft;
-
-
-                bool wall = Arena.IsWall(CurrentPlayerPositionLeft, CurrentPlayerPositionTop);
-
-                if (CurrentPlayerPositionTop == 1 || CurrentPlayerPositionTop == Console.WindowHeight-2)
-                {
-                    CurrentPlayerPositionTop = OldPlayerPositionTop;
-                }
-                if (CurrentPlayerPositionLeft == 1 || CurrentPlayerPositionLeft == Console.WindowWidth - 2)
-                {
-                    CurrentPlayerPositionLeft = OldPlayerPositionLeft;
-                }
-                Console.Write("@");
-
-
             } while (input.Key != ConsoleKey.Escape);
-
-            
         }
     }
 }

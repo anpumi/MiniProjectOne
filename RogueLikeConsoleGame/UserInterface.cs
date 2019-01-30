2a44a0b52639dcfebe3690319f1091c0a1e64fb1
anpumi <annukka.puotiniemi@gmail.com>
2019-01-30 17:48:56 +0200

    Added UI class

---
+++ RogueLikeConsoleGame/UserInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueLikeConsoleGame
+{
+    class UserInterface
+    {
+        
+        public void BuildUI()
+        {
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("Player name: " + Characters.Player.name);
+            Console.WriteLine("HP: " + Characters.Player.healthPoints);
+            Console.WriteLine();
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
